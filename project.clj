501254d976c29578b449b42e5bed68df5b2fe6a1
Evan Duncan <evan.e.duncan@gmail.com>
2022-01-14 20:55:42 -0600

    Initial commit

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject gophure "0.1.0-SNAPSHOT"
+  :description "A gopher server"
+  :url "http://example.com/FIXME"
+  :license {:name "AGPL-#.0-or-later"
+            :url "https://www.gnu.org/licenses/agpl-3.0.html"}
+  :dependencies [[org.clojure/clojure "1.10.3"]]
+  :main ^:skip-aot gophure.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
---
+++ test/gophure/core_test.clj
@@ -0,0 +1,7 @@
+(ns gophure.core-test
+  (:require [clojure.test :refer :all]
+            [gophure.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
