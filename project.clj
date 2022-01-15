1a9aba2813b276a2fb0a2b624e56240b3eccc262
Evan Duncan <evan.e.duncan@gmail.com>
2022-01-14 23:33:05 -0600

    Add TCP server with basic Gopher protocol

--- project.clj
+++ project.clj
@@ -3,7 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "AGPL-#.0-or-later"
             :url "https://www.gnu.org/licenses/agpl-3.0.html"}
-  :dependencies [[org.clojure/clojure "1.10.3"]]
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [aleph "0.4.6"]
+                 [gloss "0.2.6"]
+                 [manifold "0.2.3"]]
   :main ^:skip-aot gophure.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
