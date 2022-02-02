66fb123e835c5543c1e7c49b050c3d20fa8b4cd2
Evan Duncan <evan.e.duncan@gmail.com>
2022-02-02 07:20:40 -0600

    Rename aleph.tcp import from lf -> tcp

--- src/gophure/server.clj
+++ src/gophure/server.clj
@@ -1,10 +1,10 @@
 (ns gophure.server
-  (:require [aleph.tcp :as lf]
+  (:require [aleph.tcp :as tcp]
             [gophure.gopher :as gopher]))
 
 (defn listen
   "Start a TCP server handling Gopher protocol requests"
   [handler port]
-  (lf/start-server
+  (tcp/start-server
    (fn [s info] (handler (gopher/wrap-duplex-stream gopher/codec s) info))
    {:port port}))
